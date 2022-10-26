119b0bb403ec0e30b377a04f15b058eb6018c1a3
GVafaei <vafaeiz@sheridancollege.ca>
2022-10-26 18:09:05 -0400

    first commit

---
+++ src/cardgame/Card.java
@@ -0,0 +1,32 @@
+package cardgame;
+
+public class Card {
+
+	private int value;
+	private String suit;
+
+	public int getValue() {
+		return this.value;
+	}
+
+	/**
+	 * 
+	 * @param value
+	 */
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	public String getSuit() {
+		return this.suit;
+	}
+
+	/**
+	 * 
+	 * @param suit
+	 */
+	public void setSuit(String suit) {
+		this.suit = suit;
+	}
+
+}
